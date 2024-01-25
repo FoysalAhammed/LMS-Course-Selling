9f1d09fb663b173b1bbdafd470ed28a37154e6ab
FoysalAhammed <hanif.mia2233@gmail.com>
2024-01-26 02:06:49 +0600

    sidebar pushed

--- app/(dashboard)/_components/sidebar-routes.tsx
+++ app/(dashboard)/_components/sidebar-routes.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-import { BarChart, Compass, Layout, List } from "lucide-react";
+import { BarChart, Compass, Layout, List,Bot,MessageSquareDashedIcon } from "lucide-react";
 import { usePathname } from "next/navigation";
 
 import { SidebarItem } from "./sidebar-item";
@@ -16,6 +16,16 @@ const guestRoutes = [
     label: "Browse",
     href: "/search",
   },
+  {
+    icon: Bot,
+    label: "Support",
+    href: "/",
+  },
+  {
+    icon: MessageSquareDashedIcon,
+    label: "Notice",
+    href: "/",
+  },
 ];
 
 const teacherRoutes = [
@@ -28,6 +38,11 @@ const teacherRoutes = [
     icon: BarChart,
     label: "Analytics",
     href: "/teacher/analytics",
+  },
+  {
+    icon: MessageSquareDashedIcon,
+    label: "Notification",
+    href: "/",
   },
 ]
 
